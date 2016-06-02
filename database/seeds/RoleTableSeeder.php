6f55c41c026cf4b25ee8a74d26b9707ef0b508b5
lembarek <lembarek2011@gmail.com>
2016-06-02 18:53:39 +0100

    add migration and asset from lembarek packages

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Lembarek\Role\Models\Role;
+use Lembarek\Role\Models\Permission;
 
 class RoleTableSeeder extends Seeder
 {
@@ -12,9 +13,18 @@ class RoleTableSeeder extends Seeder
     */
     public function run()
     {
-            $roles = ['admin', 'owner', 'developper', 'designer'];
-            foreach($roles as $role){
-                Role::create(['name' => $role]);
+        $roles = [
+            'admin' => ['delete user', 'add user'],
+            'owner' => ['delete admin', 'add admin'],
+            'developper'  => ['see users'],
+            'designer' => ['see users']
+        ];
+            foreach($roles as $role => $permissions){
+                $r  = Role::create(['name' => $role]);
+                foreach($permissions as $permission){
+                    $p = Permission::create(['name' => $permission]);
+                    $r->assignPermission($p);
+                }
             }
     }
 }
